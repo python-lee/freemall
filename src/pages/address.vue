1ef84bdac1301a417af82e3487aaea5344d44918
python_lee <851868265@qq.com>
2020-05-02 16:40:07 +0800

    基础架构设计

---
+++ src/pages/address.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		this is address
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'addr'
+	}
+</script>
+
+<style>
+</style>
---
+++ src/router.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Cart from './pages/cart'
+import Address from './pages/address'
+Vue.use(Router);
+
+export default new Router({
+	routes: [
+		{
+			path: '/',
+			name: 'cart',
+			component:Cart
+		},
+		{
+			path: '/address',
+			name: 'address',
+			component: Address
+		}
+	]
+})
