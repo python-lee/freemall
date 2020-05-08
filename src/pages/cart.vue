352bef01c41ebf3c26c343715767fc36c2ce71e1
python_lee <851868265@qq.com>
2020-05-08 22:22:49 +0800

    2-3过滤器和事件的使用

--- src/pages/cart.vue
+++ src/pages/cart.vue
@@ -52,7 +52,7 @@
 							<li v-for='item in cartList' :key='item.productId'>
 								<div class="cart-tab-1">
 									<div class="cart-item-check">
-										<a href="javascipt:;" class="checkbox-btn item-check-btn" :class='{"checked":item.checked}'>
+										<a href="javascipt:;" class="checkbox-btn item-check-btn" :class='{"checked":item.checked}' @click='editCart("checked", item)'>
 											<svg class="icon icon-ok">
 												<use xlink:href="#icon-ok"></use>
 											</svg>
@@ -72,15 +72,15 @@
 									<div class="item-quantity">
 										<div class="select-self select-self-open">
 											<div class="select-self-area">
-												<a class="input-sub">-</a>
+												<a class="input-sub" @click='editCart("minus", item)'>-</a>
 												<span class="select-ipt">{{item.productNum}}</span>
-												<a class="input-add">+</a>
+												<a class="input-add" @click='editCart("add", item)'>+</a>
 											</div>
 										</div>
 									</div>
 								</div>
 								<div class="cart-tab-4">
-									<div class="item-price-total">￥{{item.productPrice*item.productNum}}元</div>
+									<div class="item-price-total">{{(item.productPrice*item.productNum) | currency}}</div>
 								</div>
 								<div class="cart-tab-5">
 									<div class="cart-item-opration">
@@ -144,6 +144,12 @@
 		mounted(){
 			this.init(); // 初始化购物车列表
 		},
+		filters: {
+			currency(value){
+				if(!value)return 0.00;
+				return '￥' + value.toFixed(2)+'元';
+			}
+		},
 		methods:{
 			init(){
 				this.axios.get('/mock/cart.json').then((response)=>{
@@ -151,6 +157,15 @@
 					let res = response.data;
 					this.cartList = res.data;
 				})
+			},
+			editCart(type, item){
+				if (type === 'add') {
+					item.productNum++;
+				} else if (type === 'minus') {
+					item.productNum--;
+				} else {
+					item.checked = !item.checked;
+				}
 			}
 		}
 	}
